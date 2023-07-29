27a4f2aef604e4555a7bdde79ab99d6d200a89e7
Evhen <postal55553@gmail.com>
2023-07-29 18:35:59 +0200

    edit

--- obj/Debug/Контроль задач_Content.g.cs
+++ obj/Debug/Контроль задач_Content.g.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute(".gitignore")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mynotesdb.mdf")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mynotesdb_log.ldf")]
 
